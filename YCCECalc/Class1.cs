0eb3696e8f5b0fea19d631bcb3cb05aa97ba0db8
Sachin Bisen <sachinbisen664@gmail.com>
2026-01-29 12:07:03 +0530

    Hii

---
+++ YCCECalc/Class1.cs
@@ -0,0 +1,26 @@
+﻿namespace YCCECalc;
+
+public class MyMath
+{
+    public static double Add(double a, double b)
+    {
+        return a + b;
+    }
+
+    public static double Subtract(double a, double b)
+    {
+        return a - b;
+    }
+
+    public static double Multiply(double a, double b)
+    {
+        return a * b;
+    }
+
+    public static double Divide(double a, double b)
+    {
+        if (b == 0)
+            throw new ArgumentException("Division by zero is not allowed.");
+        return a / b;
+    }
+}
